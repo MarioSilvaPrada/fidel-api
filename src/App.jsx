897f15e3475df93b591c0ccdec5668b2be74e09d
Mário Prada <mariosilvaprada@gmail.com>
2020-02-11 17:28:39 +0000

    adding

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <>
+    <h1>Hello from the other side!</h1>
+    <p>Page1</p>
+    <p>Page2</p>
+  </>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [ 'babel-loader' ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html', //source html
+    }),
+  ],
+};
