e4ef163a255aa8cd39a5cfa44d0eb0d559a57c13
Mário Prada <mariosilvaprada@gmail.com>
2020-03-30 11:12:22 +0100

    finishing api fetch

--- src/routes/App/App.jsx
+++ src/routes/App/App.jsx
@@ -4,13 +4,15 @@ import moment from 'moment';
 
 import * as S from './App.styled';
 
+
 const App = () => {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     api.get().then((res) => {
-      setData(res.data.items);
+      setData(res.data);
+      console.log(res.data);
       setIsLoading(false);
     });
   }, []);
@@ -20,11 +22,25 @@ const App = () => {
       {isLoading ? (
         <p>Loading</p>
       ) : (
-        data.map(({ created, id }) => (
+        data.items.map(({
+          datetime, id, amount, card,
+        }) => (
           <div key={id}>
             <p>
               created at:
-              {moment(created).format('MMMM Do YYYY')}
+              {moment(datetime).format('MMMM Do YYYY')}
+            </p>
+            <p>
+              €
+              {amount.toFixed(2)}
+            </p>
+            <p>
+              €
+              {card.scheme}
+            </p>
+            <p>
+              €
+              {card.lastNumbers}
             </p>
           </div>
         ))
